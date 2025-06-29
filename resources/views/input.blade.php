41db6208598d1eea508c872580226b185ead4860
Ahmet Çelik <ahmet.celik5443@gmail.com>
2025-06-29 20:22:00 +0300

    first commit 3

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -1,6 +1,6 @@
 <div class="input-group mb-3">
     <span class="input-group-text" id="basic-addon1">
-        <i style="font-size:21px" class="me-2 bi bi-arrow-clockwise" onclick="refresh(this)"></i>
+        <button onclick="refresh(this)">Yenile</button>
         <img src="{{ route('captcha.generate', ['refresh' => 0, 'type' => $type]) }}" alt="Captcha">
     </span>
     <input type="text" name="code_giris" class="form-control" placeholder="Güvenlik Kodu" aria-label="Username"
